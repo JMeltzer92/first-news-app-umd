bc8f4bdcfe45ce44c71a899a3201d119cb8df64a
Jon Meltzer <89864320+JMeltzer92@users.noreply.github.com>
2023-02-13 20:13:26 +0000

    adding index function

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask
+from flask import render_template as rt
 app = Flask(__name__)
 
-if __name__ = '__main__':
+if __name__ == '__main__':
     # start Flask test server
     app.run(debug=True, use_reloader=True)
+
+def index():
+    template = 'index.html'
+    return rt(template)
