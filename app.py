6aa237d887d00797899ce80849f7491f76723e32
Jon Meltzer <89864320+JMeltzer92@users.noreply.github.com>
2023-02-13 21:27:45 +0000

    created table

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
+import csv
 from flask import Flask
 from flask import render_template as rt
 app = Flask(__name__)
 
+def get_csv():
+    csv_path = './static/balt911.csv'
+    csv_file = open(csv_path, 'r')
+    csv_obj = csv.DictReader(csv_file)
+    csv_list = list(csv_obj)
+    return csv_list
+
 @app.route('/')
 def index():
     template = 'index.html'
-    return rt(template)
+    object_list = get_csv()
+    return rt(template, object_list=object_list)
 
 if __name__ == '__main__':
     # start Flask test server
