3cfcc7459725edd144fb88afa97bdfe57b1bc271
Jon Meltzer <89864320+JMeltzer92@users.noreply.github.com>
2023-02-13 21:48:06 +0000

    Building out detail pages

--- app.py
+++ app.py
@@ -16,6 +16,15 @@ def index():
     object_list = get_csv()
     return rt(template, object_list=object_list)
 
+@app.route('/<call_number>/')
+def detail(call_number):
+    template = 'detail.html'
+    object_list = get_csv()
+    for row in object_list:
+        if row['callNumber'] == call_number:
+            return rt(template, object=row)
+    abort(404)
+
 if __name__ == '__main__':
     # start Flask test server
     app.run(debug=True, use_reloader=True)
